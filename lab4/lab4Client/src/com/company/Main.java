c57ec115c1a866733794f2e8cff24995c7c261c9
Artsiom <axarkevich@gmail.com>
2021-09-29 01:48:04 +0300

    asd

--- lab4/lab4Client/src/com/company/Main.java
+++ lab4/lab4Client/src/com/company/Main.java
@@ -10,12 +10,18 @@ public class Main {
     private static BufferedWriter out;
 
     public static void main(String[] args) throws IOException {
-        socket = new Socket("0.0.0.0", 8080);
-        System.out.println("Соединение установлено:");
-
+        socket = new Socket("localhost", 8081);
         reader = new BufferedReader(new InputStreamReader(System.in));
         in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+        System.out.println("Соединение установлено:");
+        System.out.println("Введите имя: ");
+        String username = reader.readLine();
+        out.write(username + "\n");
+        out.flush();
+        String welcomeUser = in.readLine();
+        System.out.println(welcomeUser);
 
         while(socket.isConnected()){
             System.out.println("->");
--- lab4/lab4Server/src/com/company/Main.java
+++ lab4/lab4Server/src/com/company/Main.java
@@ -2,17 +2,30 @@ package com.company;
 
 import javax.imageio.IIOException;
 import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("dfsdfsdfdf");
+    private static int port = 8081;
+    private static ServerSocket serverSocket;
+    private static Socket socket;
 
-        try{
-            Server.main();
+    public static void main(String[] args) {
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        catch (IOException ex){
-            System.out.println(ex.getMessage());
+
+        while (true) {
+            try {
+                socket = serverSocket.accept();
+            } catch (IOException e) {
+                System.out.println("error: " + e);
+            }
+
+            new EchoThread(socket).start();
         }
     }
 }
